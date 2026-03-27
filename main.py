5858cdafc197d974a92508ecedc1268bd9467806
Hannah Nelson <nexec64@gmail.com>
2026-03-27 08:43:29 +0000

    looked deeper into the tool usage example to get down how it works.

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+#section-start setup
+#section-start print hello world
 print("hello world")
-#section-start imports
+#section-end
+#section-start import stuff
 from openai import OpenAI
 from jinja2 import Template
 from os import environ as ENVIRONMENT
-print("preload complete")
 #section-end
 #section-start set configuration constants
 OPENAI_API_BASE = ENVIRONMENT["OPENAI_API_BASE"]
 OPENAI_API_KEY = ENVIRONMENT["OPENAI_API_KEY"]
 PREFFERED_MODEL = "model.gguf"
+#section-end
+#section-start print preload complete
+print("preload complete")
+#section-end
 #section-end
 #section-start connect to completion engine
 engine = OpenAI(
@@ -28,7 +34,9 @@ messages = [
         "content":"Wake up. Its testing time. Report system status immediately."
     }
 ]
+tools = []
 prompt_string = template.render(**{"messages":messages, "add_generation_prompt":True, "enable_thinking":False})
+#print(f"{prompt_string=}")
 #section-end
 #section-start get response
 response=engine.completions.create(
